c06fe7d518440bfdf3b32493f9e8fdd74423429b
NAYEEM65 <mathnayeem054@gmail.com>
2022-09-10 00:25:32 +0600

    form updated

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'https://own-data-server.herokuapp.com/',
+    baseURL: 'https://lws-json-server-expense-tracke.herokuapp.com/',
 });
 
 export default axiosInstance;
+//https://own-data-server.herokuapp.com/
